75fc5888d26cf30816520345d48aff3057514698
kiredsolutions <105985102+kiredsolutions@users.noreply.github.com>
2023-07-18 16:49:10 -0600

    Update getEstado.php

--- php/facturacion/getEstado.php
+++ php/facturacion/getEstado.php
@@ -1,6 +1,6 @@
 <?php
 echo "
-	<option value='1'>Borrador</option>
+	<option value='1'>Pendientes</option>
 	<option value='2'>Pagadas</option>
 	<option value='4'>Crédito</option>
 	<option value='3'>Anuladas</option> 		
